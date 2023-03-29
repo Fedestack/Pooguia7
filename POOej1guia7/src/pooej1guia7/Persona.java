b714376f61bb709948ba76b2ea3dd98f69198495
Fedestack <124637861+Fedestack@users.noreply.github.com>
2023-03-29 20:05:10 -0300

    Add files via upload

---
+++ POOej1guia7/src/pooej1guia7/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooej1guia7;
+
+/**
+ *
+ * @author papa
+ */
+public class Persona {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    
+    public Persona(){
+    }
+    public Persona (String nombre) {
+        this.nombre = nombre; 
+    }
+}
